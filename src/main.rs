c02baa9f05206ae513e83e654bf46d4137b8281b
himkt <himkt@klis.tsukuba.ac.jp>
2025-01-03 00:25:22 +0900

    misc: depend on XDG_CONFIG_HOME

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ use std::fs::{self, File, create_dir_all};
 use std::path::PathBuf;
 
 fn main() {
-    let mut home_path = PathBuf::from(env::var("HOME").unwrap());
-    home_path.push(".config/kubectl-toggle_ctx");
-    if !home_path.exists() {
-        if let Err(e) = create_dir_all(&home_path) {
+    let mut config_home_path = match env::var("XDG_CONFIG_HOME") {
+        Ok(v) => PathBuf::from(v),
+        Err(_) => {
+            eprintln!("Environment variable XDG_CONFIG_HOME is not set.");
+            return;
+        }
+    };
+
+    config_home_path.push("kubectl-toggle-ctx");
+    if !config_home_path.exists() {
+        if let Err(e) = create_dir_all(&config_home_path) {
             eprintln!("Failed to create directory: {}", e);
             return;
         }
     }
 
-    let config_path = home_path.join("toggle");
+    let config_path = config_home_path.join("hide");
     if config_path.exists() {
         if let Err(e) = fs::remove_file(&config_path) {
             eprintln!("Failed to delete file: {}", e);
